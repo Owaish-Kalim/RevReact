ebddbdd39ee703bcade9d84f65066c99f167543b
Owaish Kalim <owaishkalim@gmail.com>
2021-01-03 19:49:19 +0530

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react' ;
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
 import Main from './components/MainComponent';
-import { DISHES } from './shared/dishes';
 import './App.css';
 
 
