0fcceb8b78b8f5be5dfb364b2ecc4f1cf98f3b3a
Owaish Kalim <owaishkalim@gmail.com>
2021-01-03 23:08:22 +0530

    Assignment-2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
@@ -49,6 +50,7 @@ class Main extends Component {
         <Header />
         <Switch>
             <Route path='/home' component={HomePage} />
+            <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route exact path='/contactus' component={Contact} />
             <Route path='/menu/:dishId' component={DishWithId} />
